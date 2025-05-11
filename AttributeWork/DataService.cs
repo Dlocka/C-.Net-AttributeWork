3e5c10d22474ffe846797c1b098c19c3b95cd22f
Dlocka <1076516565@qq.com>
2025-05-11 10:42:11 +0100

    FIrst Upload

---
+++ AttributeWork/DataService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations.Schema;
+using SQLServerDAL;
+using Models;
+namespace AttributeWork
+{
+    public static class DataService
+    {
+        /// <summary>
+        /// Search all items of a kind
+        /// </summary>
+        /// <typeparam name="SearchModel"></typeparam>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public static DataTable GetDataTable<SearchModel>() where SearchModel : BaseModel
+        {
+            var tableNameAttribute = (TableNameAttribute)GetAttribute(typeof(SearchModel), typeof(TableNameAttribute));
+            //获取string表名
+            string TableName = tableNameAttribute.TableName;
+            //创建相应model，数据表
+           
+            return new DataTable(TableName);
+        }
+        /// <summary>
+        /// 根据Id，获取一条数据
+        /// </summary>
+        /// <typeparam name="SearchModel">搜索的类型</typeparam>
+        /// <param name="_Id"></param>
+        /// <returns></returns>
+        public static SearchModel GetModel<SearchModel>(int _Id)where SearchModel : BaseModel
+            {
+            var tableNameAttribute = (TableNameAttribute)GetAttribute(typeof(SearchModel), typeof(TableNameAttribute));
+            //获取string表名
+            string TableName = tableNameAttribute.TableName;
+            string query = $"Select *from {TableName} where id=@id";
+            
+           
+            //创建相应model
+            SearchModel model =Activator.CreateInstance(typeof(SearchModel)) as SearchModel;
+              return model;
+            
+            }
+        /// <summary>
+        /// 更新某一条数据
+        /// </summary>
+        /// <typeparam name="RefreshModel"></typeparam>
+        /// <param name="_refreshModel"></param>
+        /// <returns></returns>
+        public static bool RefreshSingleData<RefreshModel>(RefreshModel _refreshModel) where RefreshModel : BaseModel
+        {
+            bool IsSuccess=false;
+            //获取表名
+            var tableNameAttribute = (TableNameAttribute)GetAttribute(typeof(RefreshModel), typeof(TableNameAttribute));
+            //创建相应model
+            string TableName = tableNameAttribute.TableName;
+            //获取数据
+            return IsSuccess;
+        }
+      
+
+        /// <summary>
+        /// 获取Attribute
+        /// </summary>
+        /// <param name="memberInfo"></param>
+        /// <param name="AttributeType"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        private static Attribute GetAttribute(MemberInfo memberInfo, Type AttributeType)
+        {
+            if (memberInfo == null || AttributeType == null)
+            {
+                throw new ArgumentNullException("MemberInfo and attributeType parameters cannot be null");
+            }
+            // Get the attribute applied to the class
+            var attribute = Attribute.GetCustomAttribute(memberInfo, AttributeType);
+            if (attribute != null)
+            {
+                Console.WriteLine($"Got Attribute:{AttributeType.Name}");
+                return attribute;
+            }
+            else
+            {
+                Console.WriteLine($"Not Got Attribute");
+                return attribute;
+            }
+        }
+
+
+    }
+}
---
+++ AttributeWork/Program.cs
@@ -0,0 +1,78 @@
+﻿using IDAL;
+using Models;
+using SQLServerDAL;
+using ExpressionExtend;
+using System.Linq.Expressions;
+using System.Reflection;
+namespace AttributeWork
+{
+    internal class Program
+    {
+        public static void update(CompanyModel company, PropertyInfo changeProperty, object value)
+        {
+
+        }
+        static void Main(string[] args)
+       {
+            IServiceDAL serviceDAL = ServiceFactory.CreateUserService();
+
+            //var properties = typeof(UserModel).GetProperties();
+            //foreach (var property in properties)
+            //{
+            //    Console.WriteLine(property.Name);
+            //}
+            Console.WriteLine("Hello, World!");
+            #region 测试查询
+            //UserModel model = serviceDAL.GetDefaultValue<UserModel>("Id", 10001);
+            //Console.WriteLine(model.Id);
+            //Console.WriteLine(model.status.ToString());
+            #endregion
+
+            #region 插入数据
+            //List<CompanyModel> companyModels = new List<CompanyModel>() {
+            //    new CompanyModel() { Name="澜起科技",Remark="科技公司"} ,
+            //    new CompanyModel() { Name="腾讯控股",Remark="科技公司"} };
+            //int affectedRows = serviceDAL.Insert(companyModels);
+            //Console.WriteLine("AffectedRows:" + affectedRows.ToString());
+            #endregion
+            //#region 更新数据
+            //CompanyModel companymodel = new CompanyModel() { Id=10012,Name = "值得买" ,Remark="笑笑笑"};
+            //int AffectedRows = serviceDAL.UpdateModel(companymodel);
+            //Console.WriteLine(AffectedRows.ToString());
+            //Console.ReadKey();
+            //#endregion
+
+            #region
+            //Expression<Func<CompanyModel, bool>> lamda = x => x.Name == "腾讯控股" || x.Name == "澜起科技";
+            //List<CompanyModel> companies = new List<CompanyModel>();
+            //int DeleteResult=DALService.DeleteBatch(companies.AsQueryable(), lamda);
+            //Console.WriteLine("Deleted rows:"+DeleteResult.ToString());
+            #endregion
+
+            #region
+
+            
+            Expression<Func<CompanyModel, bool>> Condition = x => x.Remark == "科技公司";
+            CompanyModel company = new CompanyModel();
+            PropertyInfo changeProperty = typeof(CompanyModel).GetProperty("Name");
+            company.Name = company.Name + 500;
+
+            var parameter = Expression.Parameter(typeof(CompanyModel), "company");
+            // 2. 创建属性的 MemberExpression
+            var property = Expression.Property(parameter, "Amount");
+            var ConstantExpression = Expression.Constant(123);
+            var addExpression= Expression.Add(property, ConstantExpression);
+            var assignExpression = Expression.Assign(property, addExpression);
+
+            var lambda = Expression.Lambda<Action<CompanyModel>>(assignExpression, parameter);
+
+            //Expression<Action<CompanyModel>> ActionExpression = (company)=> update(company, changeProperty, company.Name+"1234");
+           int result = serviceDAL.UpdateBatch<CompanyModel>(Condition, lambda);
+           //Console.WriteLine("UpdateRows:" + update.ToString());
+            #endregion
+
+        }
+        
+     }
+
+}
